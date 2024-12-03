47aba68a8ad282f7b5a6b606342ce83e026228e3
Vanessa Lima <vlml2001lima@gmail.com>
2024-12-03 14:51:40 -0300

    colisão

--- FlapBird.py
+++ FlapBird.py
@@ -115,3 +115,19 @@ class Cano:
     def desenhar(self, tela):
         tela.blit(self.CANO_TOPO,(self.pos_topo))
         tela.blit(self.CANO_BASE,(self.pos_base))
+
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        topo_mask = pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+
+        distancia_topo = (self.x - round(passaro.x), self.pos_topo - round(passaro.y))
+        distancia_base = (self.x - round(passaro.x), self.pos_base - round(passaro.y))
+
+        topo_ponto = passaro_mask.overlap(topo_mask, distancia_topo)
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        if base_ponto or topo_ponto:
+            return True
+        else:
+            return False
