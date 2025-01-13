4e973d77f70e94295052bcb88614a9300215fee4
Vanessa Lima <vlml2001lima@gmail.com>
2025-01-13 00:57:18 -0300

    movimento e pontuação

--- FlapBird.py
+++ FlapBird.py
@@ -192,4 +192,33 @@ def main():
                     for passaro in passaros:
                         passaro.pular()
 
+        for passaro in passaros:
+            passaro.mover()
+        chao.mover()
+
+        adicionar_cano = False
+        remover_canos = []
+
+        for cano in canos:
+            for i, passaro in enumerate(passaros):
+                if cano.colidir(passaro):
+                    passaros.pop(i)
+                if (not cano.passou) and passaro.x > cano.x:
+                    cano.passou = True
+                    adicionar_cano = True
+            cano.mover()
+            if cano.x + cano.CANO_TOPO.get_width() < 0:
+                remover_canos.append(cano)
+
+        if adicionar_cano:
+            pontos += 5
+            canos.append(Cano(600))
+
+        for cano in remover_canos:
+            canos.remove(cano)
+
+        for i, passaro in enumerate(passaros):
+            if (passaro.y + passaro.imagem.get_height()) > chao.y or passaro.y < 0:
+                passaros.pop(i)
+
         desenhar_tela(tela, passaros, canos, chao, pontos)
